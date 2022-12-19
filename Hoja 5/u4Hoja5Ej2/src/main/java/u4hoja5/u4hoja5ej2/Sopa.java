4e11f63b7d7ea7f491243765f395532792f217e3
ERzLn <eloy.ruizlinares@iesmiguelherrero.com>
2022-12-19 15:51:47 +0100

    Hoja 5

--- Hoja 5/u4Hoja5Ej2/src/main/java/u4hoja5/u4hoja5ej2/Sopa.java
+++ Hoja 5/u4Hoja5Ej2/src/main/java/u4hoja5/u4hoja5ej2/Sopa.java
@@ -14,38 +14,54 @@ public class Sopa {
 
     public Sopa() {
         arr = new char[10][20];
-        for(int x=0;x<arr.length;x++){
-            for(int y=0;y<arr[x].length;y++){
-                arr[x][y]=(char)((int)(Math.floor(Math.random()*(90-65+1))+65));
-    }}}
-    public Sopa(int filas, int columnas){
-        arr=new char[filas][columnas];
-        for(int x=0;x<arr.length;x++){
-            for(int y=0;y<arr[x].length;y++){
-                arr[x][y]=(char)((int)(Math.floor(Math.random()*(90-65+1))+65));
-    }}}
-    public void mostrar(){
-        for(int i =0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
-                System.out.print(arr[i][j]);
-            }System.out.println();
-        }System.out.println("");
-                    
+        for (int x = 0; x < arr.length; x++) {
+            for (int y = 0; y < arr[x].length; y++) {
+                arr[x][y] = (char) ((int) (Math.floor(Math.random() * (90 - 65 + 1)) + 65));
+            }
+        }
     }
-    public void setPalabra(String palabra, int x,int y,int D){
-        palabra=palabra.toUpperCase();
+
+    public Sopa(int filas, int columnas) {
+        arr = new char[filas][columnas];
+        for (int x = 0; x < arr.length; x++) {
+            for (int y = 0; y < arr[x].length; y++) {
+                arr[x][y] = (char) ((int) (Math.floor(Math.random() * (90 - 65 + 1)) + 65));
+            }
+        }
+    }
+
+    public void mostrar() {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+
+    public void setPalabra(String palabra, int x, int y, int D) {
+        palabra = palabra.toUpperCase();
         switch (D) {
             case 1 -> {
-                for(int i=0;i<palabra.length();i++){
-                    arr[x+i][y]=palabra.charAt(i);
+                if (x + palabra.length() <= arr.length) {
+                    for (int i = 0; i < palabra.length(); i++) {
+                        arr[x + i][y] = palabra.charAt(i);
+                    }
+                } else {
+                    System.out.println("La palabra no cabe!!");
                 }
             }
             case -1 -> {
-                for(int i=0;i<palabra.length();i++){
-                    arr[x][y+i]=palabra.charAt(i);
+                if (y + palabra.length() <= arr[x].length) {
+                    for (int i = 0; i < palabra.length(); i++) {
+                        arr[x][y + i] = palabra.charAt(i);
+                    }
+                } else {
+                    System.out.println("La palabra no cabe!!");
                 }
+
             }
-            default -> System.out.println("Introduce un valor correcto para posicion horizontal o vertical.");
         }
-    }
-}
+    }}
