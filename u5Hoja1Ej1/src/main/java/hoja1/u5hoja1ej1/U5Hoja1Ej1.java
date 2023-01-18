13f88faa412dfafc25525a453fccd23318283bb9
ERzLn <eloy.ruizlinares@iesmiguelherrero.com>
2023-01-18 17:33:10 +0100

    Hoja 1

---
+++ u5Hoja1Ej1/src/main/java/hoja1/u5hoja1ej1/U5Hoja1Ej1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package hoja1.u5hoja1ej1;
+
+/**
+ *
+ * @author DAW122
+ */
+public class U5Hoja1Ej1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
