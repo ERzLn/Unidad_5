a643446f0518f802d1a62f4d6a5e58c5f22521fe
ERzLn <eloy.ruizlinares@iesmiguelherrero.com>
2023-01-30 16:25:49 +0100

    Hoja 4

---
+++ u5Hoja4Ej1/src/main/java/hoja4/u5hoja4ej1/Disco.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hoja4.u5hoja4ej1;
+
+/**
+ *
+ * @author DAW122
+ */
+public class Disco {
+    
+}
