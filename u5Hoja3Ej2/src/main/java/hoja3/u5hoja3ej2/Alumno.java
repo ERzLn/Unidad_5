da46649add586296b999728b78c0fb84d96e17e6
ERzLn <eloy.ruizlinares@iesmiguelherrero.com>
2023-01-30 15:51:59 +0100

    Hoja 3

---
+++ u5Hoja3Ej2/src/main/java/hoja3/u5hoja3ej2/Alumno.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hoja3.u5hoja3ej2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAW122
+ */
+public class Alumno extends Persona {
+    private int [] notas;
+
+    public Alumno(int numNotas) {
+        notas=new int [numNotas];
+    }
+    public String mostrar(){
+        String mostrar="";
+        int media=0;
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Introduce la nota "+(i+1)); 
+            notas[i]=new Scanner(System.in).nextInt();
+            media+=notas[i];                        
+        }
+        mostrar+="DNI: "+super.getDni()+" Direccion: "+super.getDireccion()+ " Nombre alumno: "+super.getNombre()+" Media: "+media+"\n";
+        return mostrar;
+    }
+    
+}
