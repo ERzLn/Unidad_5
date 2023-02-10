26ffa8d810aa5dfa5e72f37a21384c5ced9c2a87
ERzLn <eloy.ruizlinares@iesmiguelherrero.com>
2023-02-10 17:22:48 +0100

    Hoja 7

--- Hoja7/U5Hoja7Ej3/src/main/java/hoja7/u5hoja7ej3/U5Hoja7Ej3.java
+++ Hoja7/U5Hoja7Ej3/src/main/java/hoja7/u5hoja7ej3/U5Hoja7Ej3.java
@@ -4,6 +4,7 @@
  */
 package hoja7.u5hoja7ej3;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -15,21 +16,25 @@ public class U5Hoja7Ej3 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int numTeclado;
-        boolean control=false;
+        boolean control = false;
         System.out.println("Introduce un numero:");
         do {
-        try {
-            numTeclado = new Scanner(System.in).nextInt();
-            if (numTeclado < 0 || numTeclado > 100) {
-                throw new Exception("El numero no se encuentra en un rango entre 0 y 100");
+            try {
+                numTeclado = new Scanner(System.in).nextInt();
+                if (numTeclado < 0 || numTeclado > 100) {
+                    throw new Exception("El numero no se encuentra en un rango entre 0 y 100");
+                } else {
+                    System.out.println("El numero se encuentra en el rango correcto");
+                }
+                control = true;
+
+            } catch (InputMismatchException ex) {
+                System.out.println("error no es un numero valido");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("Intruduzca el numero de nuevo:");
+                control = false;
             }
-            else System.out.println("El numero se encuentra en el rango correcto");
-            control=true;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Intruduzca el numero de nuevo:");
-            control=false;
-        }
         } while (!control);
     }
 }
