bd94b3f2d99a22e6fad35b720a33e0fb7aa8e1f7
ERzLn <eloy.ruizlinares@iesmiguelherrero.com>
2023-02-10 16:24:46 +0100

    Hoja 7

---
+++ Hoja7/U5Hoja7Ej3/src/main/java/hoja7/u5hoja7ej3/U5Hoja7Ej3.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package hoja7.u5hoja7ej3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAW122
+ */
+public class U5Hoja7Ej3 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numTeclado;
+        boolean control=false;
+        System.out.println("Introduce un numero:");
+        do {
+        try {
+            numTeclado = new Scanner(System.in).nextInt();
+            if (numTeclado < 0 || numTeclado > 100) {
+                throw new Exception("El numero no se encuentra en un rango entre 0 y 100");
+            }
+            else System.out.println("El numero se encuentra en el rango correcto");
+            control=true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Intruduzca el numero de nuevo:");
+            control=false;
+        }
+        } while (!control);
+    }
+}
