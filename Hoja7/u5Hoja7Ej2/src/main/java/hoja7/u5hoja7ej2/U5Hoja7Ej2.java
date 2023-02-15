6e11d618296481f169e529bc00e1d154c156303c
ERzLn <eloy.ruizlinares@iesmiguelherrero.com>
2023-02-15 17:32:25 +0100

    Excepciones

--- Hoja7/u5Hoja7Ej2/src/main/java/hoja7/u5hoja7ej2/U5Hoja7Ej2.java
+++ Hoja7/u5Hoja7Ej2/src/main/java/hoja7/u5hoja7ej2/U5Hoja7Ej2.java
@@ -4,6 +4,7 @@
  */
 package hoja7.u5hoja7ej2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -23,17 +24,23 @@ public class U5Hoja7Ej2 {
                 System.out.print("Ingrese un número entero: ");
                 a = new Scanner(System.in).nextInt();
                 System.out.println("Introduce el divisor: ");
-                b = new Scanner(System.in).nextInt();                
+                b = new Scanner(System.in).nextInt();
+                if (b==0) {
+                    throw new ArithmeticException ("Division invalida. No se puede dividir entre 0.");
+                }
                 System.out.println("El resultado es: " + (a / b));
                 x = false;
 
             } catch (ArithmeticException e) {
-                System.out.println("Division invalida. No se puede dividir entre 0.");
+                System.out.println(e.getMessage());
                 
             }
-            catch (Exception e) {                
+            catch (InputMismatchException e) {                
                 System.out.println("El valor introducido no es un entero.");
                 System.out.println("Introduce correctamente los valores:");
+            }catch (Exception e){
+                System.out.println("Error desconocido");
+                System.out.println(e.getMessage());
             }
 
         }
