4a452d1e21d39d506fc50baba65464dd80334066
ERzLn <eloy.ruizlinares@iesmiguelherrero.com>
2023-02-15 17:33:43 +0100

    Excepciones

--- Hoja7/U5Hoja7Ej3/src/main/java/hoja7/u5hoja7ej3/U5Hoja7Ej3.java
+++ Hoja7/U5Hoja7Ej3/src/main/java/hoja7/u5hoja7ej3/U5Hoja7Ej3.java
@@ -16,25 +16,25 @@ public class U5Hoja7Ej3 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int numTeclado;
-        boolean control = false;
-        System.out.println("Introduce un numero:");
+        boolean valido = false;
+
         do {
             try {
+                System.out.println("Introduce un numero:");
                 numTeclado = new Scanner(System.in).nextInt();
                 if (numTeclado < 0 || numTeclado > 100) {
                     throw new Exception("El numero no se encuentra en un rango entre 0 y 100");
                 } else {
                     System.out.println("El numero se encuentra en el rango correcto");
                 }
-                control = true;
+                valido = true;
 
             } catch (InputMismatchException ex) {
                 System.out.println("error no es un numero valido");
             } catch (Exception e) {
                 System.out.println(e.getMessage());
-                System.out.println("Intruduzca el numero de nuevo:");
-                control = false;
+
             }
-        } while (!control);
+        } while (!valido);
     }
 }
