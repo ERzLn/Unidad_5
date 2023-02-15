6f89b32d5ff0dc42b298d1935db3576e28b19627
ERzLn <eloy.ruizlinares@iesmiguelherrero.com>
2023-02-15 18:53:44 +0100

    Excepciones

---
+++ Hoja 9/u5Hoja9Ej2/src/main/java/hoja9/u5hoja9ej2/Prueba.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hoja9.u5hoja9ej2;
+
+/**
+ *
+ * @author DAW122
+ */
+public class Prueba {
+    private String string;
+
+    public String getString() {
+        return string;
+    }
+}
---
+++ Hoja 9/u5Hoja9Ej2/src/main/java/hoja9/u5hoja9ej2/U5Hoja9Ej2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package hoja9.u5hoja9ej2;
+
+/**
+ *
+ * @author DAW122
+ */
+public class U5Hoja9Ej2 {
+
+    public static void main(String[] args) {
+        Prueba pru=new Prueba();
+        pru=null;
+        try {
+            pru.getString();
+        } catch (NullPointerException e){
+            System.out.println(e.getMessage());
+            System.out.println("El objeto es nulo");
+        }
+    }
+}
